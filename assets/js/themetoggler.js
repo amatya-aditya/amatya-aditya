84012ef68285ed110a7ac2e44ee06dbd81666363
Aditya <63241408+amatya-aditya@users.noreply.github.com>
2022-01-05 04:33:22 +0545

    theme-toggler and color update

--- assets/js/themetoggler.js
+++ assets/js/themetoggler.js
@@ -1,83 +1,41 @@
-// const chk = document.getElementById('chk');
 
-// chk.addEventListener('change', (e) => {
-//     sessionStorage.setItem('dark-mode', e.target.checked);
-//     if ( e.target.checked )
-//       document.body.classList.add('dark');
-//     else
-//       document.body.classList.remove('dark');
-    
-// });
+  
+//  function getTheme() {
+//   return localStorage.getItem('theme') || 'light';
+// }
+// function saveTheme(theme) {
+//   localStorage.setItem('theme', theme);
+// }
 
-// window.addEventListener('load', () => {
-//     let value = sessionStorage.getItem('dark-mode');
-//     document.getElementById('chk').checked = value === 'true' ? true : false;
-//     if ( value === 'true' )
-//       document.body.classList.add('dark');
-//     else
-//       document.body.classList.remove('dark');
-// })
+// const colorScheme = document.querySelector('meta[name="color-scheme"]');
+// function applyTheme(theme) {
+//   document.body.className = theme;
+//   colorScheme.content = theme;
+// }
+
+// function rotateTheme(theme) {
+//   if (theme === 'light') {
+//     return 'dark'
+//   }
+//   return 'light';
+// }
+
+// const themeDisplay = document.getElementById('theme');
+// const themeToggler = document.getElementById('theme-toggle');
 
 
-  /*------------ Cookie functions and color js ------------*/
-  function createCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-      var date = new Date();
-      date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-      expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-  }
 
-  function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == " ") c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
+// setTimeout(() => {
+//   let theme = getTheme();
+//   applyTheme(theme);
 
-  function eraseCookie(name) {
-    createCookie(name, "", -1);
-  }
 
-  var prefersDark =
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: main-default)").matches;
-  var selectedNightTheme = readCookie("dark");
+//   themeToggler.onclick = () => {
+//     const newTheme = rotateTheme(theme);
+//     applyTheme(newTheme);
 
-  if (
-    selectedNightTheme == "true" ||
-    (selectedNightTheme === null && prefersDark)
-  ) {
-    applyNight();
-    document.getElementById('chk').prop("checked", true);
-  } else {
-    applyDay();
-    document.getElementById('chk').prop("checked", false);
-  }
+//     saveTheme(newTheme);
 
-  function applyNight() {
-    $("body").addClass("dark");
-  }
-
-  function applyDay() {
-    $("body").removeClass("dark");
-  }
-
-  document.getElementById('chk').change(function () {
-    if ($(this).is(":checked")) {
-      applyNight();
-      $(".tab-btns").css("color", "#6B707F");
-      createCookie("dark", true, 999);
-    } else {
-      applyDay();
-      $(".tab-btns").css("color", "#007CBA");
-      createCookie("dark", false, 999);
-    }
-  });
-
+//     theme = newTheme;
+//   }
+// }, 1000);
